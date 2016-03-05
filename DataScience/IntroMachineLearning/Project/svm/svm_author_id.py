0d3e80580a9a09c40be3feb47117c40a3127e355
Mark Mavromatis <markmavromatis@Marks-iMac.attlocal.net>
2016-03-04 16:39:43 -0800

    Lesson 2 MiniProject - Output element predictions

--- DataScience/IntroMachineLearning/Project/svm/svm_author_id.py
+++ DataScience/IntroMachineLearning/Project/svm/svm_author_id.py
@@ -25,6 +25,35 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+# Speed up the training period by training on a smaller dataset.
+#features_train = features_train[:len(features_train) / 100]
+#labels_train = labels_train[:len(labels_train) / 100]
+
+
+from sklearn.svm import SVC
+
+#clf = SVC(kernel="linear")
+
+# Try the complex RBF kernel
+clf = SVC(C=10000, kernel="rbf")
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training time: ", round(time() - t0, 3), "s")
+t0 = time()
+pred = clf.predict(features_test)
+print("Prediction time: ", round(time() - t0, 3), "s")
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print("Accuracy is: " + str(accuracy))
+
+
+#Prediction individual elements
+print("Element #10 = " + str(pred[10]))
+print("Element #26 = " + str(pred[26]))
+print("Element #50 = " + str(pred[50]))
+
+print(len(filter(lambda x: x == 1, pred)))
 #########################################################
 
 
