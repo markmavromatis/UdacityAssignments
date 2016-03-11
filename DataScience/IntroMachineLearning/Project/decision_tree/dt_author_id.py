57cdf06daf03b1c4fde74c0844e244f73c917c17
Mark Mavromatis <markmavromatis@Marks-iMac.attlocal.net>
2016-03-10 17:24:52 -0800

    Intro to Machine Learning - Decision Trees

--- DataScience/IntroMachineLearning/Project/decision_tree/dt_author_id.py
+++ DataScience/IntroMachineLearning/Project/decision_tree/dt_author_id.py
@@ -26,6 +26,18 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 
+# Number of features is:
+print(len(features_train[0]))
+
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+score = accuracy_score(labels_test, pred)
+print("Accuracy score is: " + str(score))
+
 #########################################################
 
 
