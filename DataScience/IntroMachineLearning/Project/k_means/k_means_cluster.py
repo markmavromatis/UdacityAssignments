53467b1a10f22bb6b8295871d40b467d98808986
Mark Mavromatis <mmavromatis@guidewire.com>
2016-03-11 12:13:26 -0800

    Intro to Machine Learning Lesson 8 - KMeans

--- DataScience/IntroMachineLearning/Project/k_means/k_means_cluster.py
+++ DataScience/IntroMachineLearning/Project/k_means/k_means_cluster.py
@@ -11,6 +11,7 @@ import pickle
 import numpy
 import matplotlib.pyplot as plt
 import sys
+import math
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
@@ -43,13 +44,41 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r")
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
+min_stock_options = -1
+max_stock_options = 0
+
+for key in data_dict:
+    stock_options = data_dict[key]['exercised_stock_options']
+    if math.isnan(float(stock_options)) != True:
+        if min_stock_options == -1 or stock_options < min_stock_options:
+            min_stock_options = stock_options
+        if stock_options > max_stock_options:
+            max_stock_options = stock_options
+
+print("Max stock options = " + str(min_stock_options))
+print("Min stock options = " + str(max_stock_options))
+
+min_salary = -1
+max_salary= 0
+
+for key in data_dict:
+    salary = data_dict[key]['salary']
+    if math.isnan(float(salary)) != True:
+        if min_salary == -1 or salary < min_salary:
+            min_salary = salary
+        if salary > max_salary:
+            max_salary = salary
+
+print("Max salary = " + str(max_salary))
+print("Min salary= " + str(min_salary))
 
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,19 +87,29 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, f3 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+#print("Data = ")
+#print(data)
+#print("Finance Features:")
+#print(finance_features)
 
+from sklearn.cluster import KMeans
+kmeans_example = KMeans(n_clusters=2)
+kmeans_example.fit_predict(finance_features)
+pred = kmeans_example.labels_
+#pred = kmeans_example.predict()
+#print(labels)
 
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters_3features.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
