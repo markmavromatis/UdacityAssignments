92e691bd0037bdc832450b29404e041d95028ee1
Mark Mavromatis <markmavromatis@57.87.208.web-pass.com>
2016-08-25 01:12:24 -0700

    Fixed data collection and tooltip rendering. Added number formatting

--- DataScience/P6_VisualizationFlights/getAirportCarrierStatistics.py
+++ DataScience/P6_VisualizationFlights/getAirportCarrierStatistics.py
@@ -4,6 +4,8 @@ client = MongoClient()
 db = MongoClient().test
 print("Hello World")
 
+monthNames = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec'];
+
 airlineCodes = {}
 airlineCodes['AA'] = "American Airlines"
 airlineCodes['B6'] = "JetBlue Airways"
@@ -30,10 +32,9 @@ for i in months:
 
 	pipeline.extend([
 		{"$match" : {"Origin" : {"$in" : ["ATL",'ORD','DFW','DEN','LAX',
-			'SFO', 'IAH', 'PHX', 'LAS', 'MSP'] }}},
+			'SFO', 'IAH', 'PHX', 'LAS', 'MSP'] }, "ArrDel15" : 0}},
 	  	{"$group" : {"_id": {"Origin" : "$Origin", "Month" : "$Month", 
-	  		"Carrier" : "$Carrier"}, "total" : {"$sum" : 1}, "delays" : 
-	  		{"$sum" : "$DepDel15"}, "cancellations" : {
+	  		"Carrier" : "$Carrier"}, "total" : {"$sum" : 1}, "cancellations" : {
 	  		"$sum" : "$Cancelled"}, "carrierDelayMins" : {"$sum" : 
 	  		"$CarrierDelay"}, "weatherDelayMins" : {"$sum" : 
 	  		"$WeatherDelay"}, "nasDelayMins" : {"$sum" : "$NASDelay"},
@@ -41,22 +42,63 @@ for i in months:
 	  		"lateAircraftDelayMins" : {"$sum" : "$LateAircraftDelay"}}}, 
 	  	{"$sort" : {"_id" : 1}}
 	])
-	results = db.flights2015.aggregate(pipeline) 
+	results_ontime = db.flights2015.aggregate(pipeline) 
+
+	pipeline = []
+	if i is not None:
+		pipeline.append({"$match" : {"Month" : i}})
+
+	pipeline.extend([
+		{"$match" : {"Origin" : {"$in" : ["ATL",'ORD','DFW','DEN','LAX',
+			'SFO', 'IAH', 'PHX', 'LAS', 'MSP'] }, "ArrDel15" : 1}},
+	  	{"$group" : {"_id": {"Origin" : "$Origin", "Month" : "$Month", 
+	  		"Carrier" : "$Carrier"}, "total" : {"$sum" : 1}, "cancellations" : {
+	  		"$sum" : "$Cancelled"}, "arriveDelayMins" : {"$sum" : "$ArrDelay"}, "carrierDelayMins" : {"$sum" : 
+	  		"$CarrierDelay"}, "weatherDelayMins" : {"$sum" : 
+	  		"$WeatherDelay"}, "nasDelayMins" : {"$sum" : "$NASDelay"},
+	  		"securityDelayMins" : {"$sum" : "$SecurityDelay"},
+	  		"lateAircraftDelayMins" : {"$sum" : "$LateAircraftDelay"}}}, 
+	  	{"$sort" : {"_id" : 1}}
+	])
+	results_delayed = db.flights2015.aggregate(pipeline) 
 
 
 	if i is None:
-		filename = filename_base + filename_extension
+		filename = filename_base + "_annual" + filename_extension
 	else:
-		filename = filename_base + "_" + str(i) + filename_extension
+		filename = filename_base + "_" + monthNames[i - 1] + filename_extension
 
 	f = open(filename, 'w')
-	f.write("RecordType,Origin,Month,CarrierName,Count,Cancellations,CarrierDelayMins,WeatherDelayMins,NasDelayMins,SecurityDelayMins,LateAircraftDelayMins\n");
-	for document in results:
+	f.write("RecordType,Origin,Month,CarrierName,Count,ArrDelayMins,Cancellations,CarrierDelayMins,WeatherDelayMins,NasDelayMins,SecurityDelayMins,LateAircraftDelayMins\n")
+	for document in results_ontime:
 		origin = document['_id']['Origin']
 		month = str(document['_id']['Month'])
 		carrierCode = document['_id']['Carrier']
 		total = str(document['total'])
-		delays = str(document['delays'])
+		arrDelayMins = str(0)
+		cancellations = str(document['cancellations'])
+		carrierDelayMinutes = str(0)
+		weatherDelayMinutes = str(0)
+		nasDelayMinutes = str(0)
+		securityDelayMinutes = str(0)
+		lateAircraftDelayMinutes = str(0)
+		carrierName = ""
+
+		if carrierCode in airlineCodes:
+			carrierName = airlineCodes[carrierCode]
+		else:
+			print("Unknown Carrier Code: " + carrierCode)
+
+		print("Writing record to file...")
+
+		f.write("OnTime," + str(origin) + "," + str(month) + "," + carrierName + "," + str(total) + ",0," + str(cancellations) + ",0,0,0,0,0\n")
+
+	for document in results_delayed:
+		origin = document['_id']['Origin']
+		month = str(document['_id']['Month'])
+		carrierCode = document['_id']['Carrier']
+		total = str(document['total'])
+		arrDelayMins = str(document['arriveDelayMins'])
 		cancellations = str(document['cancellations'])
 		carrierDelayMinutes = str(document['carrierDelayMins'])
 		weatherDelayMinutes = str(document['weatherDelayMins'])
@@ -69,13 +111,12 @@ for i in months:
 			carrierName = airlineCodes[carrierCode]
 		else:
 			print("Unknown Carrier Code: " + carrierCode)
-
-
-		f.write("Totals," + origin + "," + month + "," + carrierName + "," + total + "\n");
-		f.write("Delays," + origin + "," + month + "," + carrierName + "," + 
-			delays + "," + cancellations + "," + carrierDelayMinutes + "," +
-			weatherDelayMinutes + "," + nasDelayMinutes + "," + 
-			securityDelayMinutes + "," + lateAircraftDelayMinutes + "\n")
+		f.write("Delayed," + origin + "," + str(month) + "," + 
+			carrierName + "," + str(total) + "," + str(arrDelayMins) + 
+			"," + str(cancellations) + "," + str(carrierDelayMinutes) +
+			"," + str(weatherDelayMinutes) + "," + str(nasDelayMinutes) +
+			"," + str(securityDelayMinutes) + "," + 
+			str(lateAircraftDelayMinutes) + "\n")
 	f.close()
 	print("Wrote output to file: " + filename)
 print("Done!")
